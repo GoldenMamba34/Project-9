3c34ee40ac161cab46722d6677b38f032a2ccc6f
Andrew Courey <goldenmambasoft@gmail.com>
2018-10-21 01:01:38 -0400

    Almost Done!

--- main.coffee
+++ main.coffee
@@ -1,16 +1,34 @@
-$("#darkMode").parent().on("click touchsstart", ->
+theme = "light"
+
+changeToDarkTheme = ->
   $("body").css("background", "rgb(10,10,10)")
   $("body").css("color", "white")
   $("h1").css("color", "white")
   $("p").css("color", "white")
   $(".modal-content").css("background", "rgb(10,10,10)")
   $(".modal-content").css("color", "white")
-  )
-$("#lightMode").parent().on("click touchsstart", ->
+  theme = "dark"
+changeToLightTheme = ->
   $("body").css("background", "white")
   $("h1").css("color", "#212529")
   $("p").css("color", "#212529")
   $(".modal-content").css("background", "white")
   $(".modal-content").css("color", "#212529")
   $("body").css("color", "unset")
+  theme = "light"
+
+$("#darkMode").parent().on("click touchsstart", ->
+  changeToDarkTheme()
+  )
+$("#lightMode").parent().on("click touchsstart", ->
+  changeToLightTheme()
+  )
+$("#saveSettings").on("click touchsstart", ->
+  localStorage.setItem('theme', theme);
+  )
+$(document).ready(->
+  if localStorage.getItem('theme') is "dark"
+    changeToDarkTheme()
+  else
+    changeToLightTheme()
   )
--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-$("#darkMode").parent().on("click touchsstart", function() {
+var changeToDarkTheme, changeToLightTheme, theme;
+
+theme = "light";
+
+changeToDarkTheme = function() {
   $("body").css("background", "rgb(10,10,10)");
   $("body").css("color", "white");
   $("h1").css("color", "white");
   $("p").css("color", "white");
   $(".modal-content").css("background", "rgb(10,10,10)");
-  return $(".modal-content").css("color", "white");
-});
+  $(".modal-content").css("color", "white");
+  return theme = "dark";
+};
 
-$("#lightMode").parent().on("click touchsstart", function() {
+changeToLightTheme = function() {
   $("body").css("background", "white");
   $("h1").css("color", "#212529");
   $("p").css("color", "#212529");
   $(".modal-content").css("background", "white");
   $(".modal-content").css("color", "#212529");
-  return $("body").css("color", "unset");
+  $("body").css("color", "unset");
+  return theme = "light";
+};
+
+$("#darkMode").parent().on("click touchsstart", function() {
+  return changeToDarkTheme();
+});
+
+$("#lightMode").parent().on("click touchsstart", function() {
+  return changeToLightTheme();
+});
+
+$("#saveSettings").on("click touchsstart", function() {
+  return localStorage.setItem('theme', theme);
+});
+
+$(document).ready(function() {
+  if (localStorage.getItem('theme') === "dark") {
+    return changeToDarkTheme();
+  } else {
+    return changeToLightTheme();
+  }
 });
